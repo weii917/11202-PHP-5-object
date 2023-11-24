bfe163731e645e4900b114a2038c8a6badd9cbfa
wei <lin951016@gmail.com>
2023-11-24 11:03:00 +0800

    繼承使用

--- index.php
+++ index.php
@@ -19,14 +19,28 @@ class Animal{
 }
 
 $animal=new Animal('阿明'); //實例化 instant
-
+/* 
 echo '顯示名稱:'.$animal->getName();
 echo "<br>";
 $animal->setName('小花');
 echo '顯示名稱:'.$animal->getName();
-echo "<br>";
+echo "<br>";*/
 /* $animal->name='阿中';
 echo '顯示名稱:'.$animal->name; */
+/* echo "<br>"; */
+
+
+class Dog extends Animal{
+
+    function sit(){
+        echo $this->name;
+        echo "坐下";
+    }
+}
+
+$dog=new Dog('阿富');
+echo $dog->getName();
 echo "<br>";
-
-
+echo $dog->setName('阿旺');
+echo $dog->getName();
+$dog->sit();
