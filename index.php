e08a1b627e0d961af404491cca48b372f5c947dd
wei <lin951016@gmail.com>
2023-11-24 13:50:12 +0800

    push

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <?php
 
-class Animal{
+class Animal
+{
     protected $name;
-
     public function __construct($name)
     {
-        $this->name=$name;
+        $this->name = $name;
     }
-
     public function setName($name){
-        $this->name=$name;
+        $this->name = $name;
     }
-
     public function getName(){
         return $this->name;
     }
+    
 
 }
 
-$animal=new Animal('阿明'); //實例化 instant
-/* 
-echo '顯示名稱:'.$animal->getName();
-echo "<br>";
-$animal->setName('小花');
-echo '顯示名稱:'.$animal->getName();
-echo "<br>";*/
-/* $animal->name='阿中';
-echo '顯示名稱:'.$animal->name; */
-/* echo "<br>"; */
+$animal = new Animal('阿明'); //實例化 instant
+
+// echo '顯示名稱:' . $animal->getName();
+// echo "<br>";
+// $animal->setName('小花');
+// echo '顯示名稱:' . $animal->getName();
+// echo "<br>";
+// $animal->name = '阿中';
+// echo '顯示名稱:' . $animal->name;
+// echo "<br>";
 
 
 class Dog extends Animal{
