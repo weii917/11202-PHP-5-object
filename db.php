f00e2bf2279f3372e2f6de7d465c00990a923407
wei <lin951016@gmail.com>
2023-11-24 16:10:32 +0800

    將upadate跟insert合併在同個function判斷有無id執行upadte或insert

--- db.php
+++ db.php
@@ -64,57 +64,28 @@ class DB{
     
     function save($array){
         if(isset($array['id'])){
-            $this->update($array['id'],$array);
+            $sql = "update `$this->table` set ";
+    
+            if (!empty($cols)) {
+                foreach ($cols as $col => $value) {
+                    $tmp[] = "`$col`='$value'";
+                }
+            } else {
+                echo "錯誤:缺少要編輯的欄位陣列";
+            }
+            $sql .= join(",", $tmp);        
+            $sql .= " where `id`='{$array['id']}'";
         }else{
-            $this->insert($array);
+            $sql = "insert into `$this->table` ";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+        
+            $sql = $sql . $cols . " values " . $vals;
         }
+        return $this->pdo->exec($sql);
     }
 
-    protected function update($cols)
-    {
-        
-    
-        $sql = "update `$this->table` set ";
-    
-        if (!empty($cols)) {
-            foreach ($cols as $col => $value) {
-                $tmp[] = "`$col`='$value'";
-            }
-        } else {
-            echo "錯誤:缺少要編輯的欄位陣列";
-        }
-    
-        $sql .= join(",", $tmp);
-        $tmp = [];
-        if (is_array($id)) {
-            foreach ($id as $col => $value) {
-                $tmp[] = "`$col`='$value'";
-            }
-            $sql .= " where " . join(" && ", $tmp);
-        } else if (is_numeric($id)) {
-            $sql .= " where `id`='$id'";
-        } else {
-            echo "錯誤:參數的資料型態比須是數字或陣列";
-        }
-        // echo $sql;
-        return $this->pdo->exec($sql);
-    }
-    
-    protected function insert($values)
-    {
-        
-    
-        $sql = "insert into `$this->table` ";
-        $cols = "(`" . join("`,`", array_keys($values)) . "`)";
-        $vals = "('" . join("','", $values) . "')";
-    
-        $sql = $sql . $cols . " values " . $vals;
-    
-        //echo $sql;
-    
-        return $this->pdo->exec($sql);
-    }
-    
+   
     function del($id)
     {
        
