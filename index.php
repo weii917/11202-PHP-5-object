964856f9bffa2dace6a7e88d64b9746ed96f881a
wei <lin951016@gmail.com>
2023-11-24 10:40:32 +0800

    建構式使用

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
 
-
 class Animal{
-    public $name;
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
 
     public function setName($name){
         $this->name=$name;
     }
 
+    public function getName(){
+        return $this->name;
+    }
+
 }
 
-$animal=new Animal; //實例化 instant
+$animal=new Animal('阿明'); //實例化 instant
 
-echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getName();
 echo "<br>";
 $animal->setName('小花');
-echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getName();
+echo "<br>";
+/* $animal->name='阿中';
+echo '顯示名稱:'.$animal->name; */
 echo "<br>";
 
 
